2e572549bd7f851dcfb08a2a1cf5375027e1b9dd
kora13481 <amelianekor@gmail.com>
2023-10-19 23:23:48 +0200

    okok

--- future/test.js
+++ future/test.js
@@ -41,6 +41,37 @@ async function updateCryptoData(symbol) {
     totalCell.textContent = `${totalValue}%`;
 
     if (totalVariation >= 0.10 && totalVariation <= 0.30) {
+      // Ajouter la classe CSS appropriée à totalCell
+      totalCell.classList.add("positive");
+    } else if (totalVariation >= -0.30 && totalVariation <= -0.10) {
+      // Ajouter la classe CSS appropriée à totalCell
+      totalCell.classList.add("negative");
+    }
+
+    if (totalVariation >= 0.10 && totalVariation <= 0.30) {
+      // Ajouter le nom de la crypto en dehors du tableau
+      const cryptoNameDiv = document.getElementById("cryptoNames");
+      const cryptoName = document.createElement("p");
+      cryptoName.textContent = `${symbol} : ${totalValue}%`;
+      cryptoNameDiv.appendChild(cryptoName);
+    
+      // Ajouter la classe CSS appropriée
+      cryptoName.classList.add("positive");
+    } else if (totalVariation >= -0.30 && totalVariation <= -0.10) {
+      // Ajouter le nom de la crypto en dehors du tableau
+      const cryptoNameDiv = document.getElementById("cryptoNames");
+      const cryptoName = document.createElement("p");
+      cryptoName.textContent = `${symbol} : ${totalValue}%`;
+      cryptoNameDiv.appendChild(cryptoName);
+    
+      // Ajouter la classe CSS appropriée
+      cryptoName.classList.add("negative");
+    }
+
+    
+
+
+    if (totalVariation >= 0.10 && totalVariation <= 0.30) {
       checkAndNotify(symbol, totalVariation);
     }
 
@@ -102,13 +133,195 @@ async function refreshData(symbol) {
 if ("Notification" in window) {
   Notification.requestPermission().then(permission => {
     if (permission === "granted") {
-      refreshData("1INCH");
+       refreshData("1INCH");
       refreshData("AAVE");
       refreshData("ACH");
       refreshData("ADA");
       refreshData("AGIX");
       refreshData("AGLD");
       refreshData("ALGO");
+      refreshData("ALICE");
+      refreshData("ALPHA");
+      refreshData("ALPACA");
+      refreshData("AMB");
+      refreshData("ANKR");
+      refreshData("ANT");
+      refreshData("APE");
+      refreshData("API3");
+      refreshData("APT");
+      refreshData("ARB");
+      refreshData("ARKM");
+      refreshData("ARPA");
+      refreshData("AR");
+      refreshData("ASTR");
+      refreshData("ATA");
+      refreshData("ATOM");
+      refreshData("AUDIO");
+      refreshData("AVAX");
+      refreshData("AXS");
+      refreshData("BAKE");
+      refreshData("BAL");
+      refreshData("BAND");
+      refreshData("BAT");
+      refreshData("BCH");
+      refreshData("BEL");
+      refreshData("BLZ");
+      refreshData("BNB");
+      refreshData("BNT");
+      refreshData("BNX");
+      refreshData("BTC");
+      refreshData("C98");
+      refreshData("CELO");
+      refreshData("CELR");
+      refreshData("CFX");
+      refreshData("CHR");
+      refreshData("CHZ");
+      refreshData("CKB");
+      refreshData("COMBO");
+      refreshData("COMP");
+      refreshData("COTI");
+      refreshData("CRV");
+      refreshData("CTK");
+      refreshData("CTSI");
+      refreshData("CVX");
+      refreshData("CYBER");
+      refreshData("DAR");
+      refreshData("DASH");
+      refreshData("DENT");
+      refreshData("DGB");
+      refreshData("DOGE");
+      refreshData("DOT");
+      refreshData("DUSK");
+      refreshData("DYDX");
+      refreshData("EDU");
+      refreshData("EGLD");
+      refreshData("ENJ");
+      refreshData("ENS");
+      refreshData("EOS");
+      refreshData("ETC");
+      refreshData("ETH");
+      refreshData("FET");
+      refreshData("FLM");
+      refreshData("FLOKI");
+      refreshData("FLOW");
+      refreshData("FTM");
+      refreshData("FXS");
+      refreshData("GALA");
+      refreshData("GAL");
+      refreshData("GMT");
+      refreshData("GMX");
+      refreshData("GRT");
+      refreshData("GTC");
+      refreshData("HBAR");
+      refreshData("HFT");
+      refreshData("HIFI");
+      refreshData("HIGH");
+      refreshData("HOT");
+      refreshData("ICP");
+      refreshData("ICX");
+      refreshData("IDEX");
+      refreshData("ID");
+      refreshData("IMX");
+      refreshData("INJ");
+      refreshData("IOST");
+      refreshData("IOTA");
+      refreshData("IOTX");
+      refreshData("JASMY");
+      refreshData("JOE");
+      refreshData("KAVA");
+      refreshData("KEY");
+      refreshData("KLAY");
+      refreshData("KNC");
+      refreshData("KSM");
+      refreshData("LDO");
+      refreshData("LEVER");
+      refreshData("LINA");
+      refreshData("LINK");
+      refreshData("LIT");
+      refreshData("LPT");
+      refreshData("LQTY");
+      refreshData("LRC");
+      refreshData("LTC");
+      refreshData("LUNA");
+      refreshData("LUNC");
+      refreshData("MAGIC");
+      refreshData("MANA");
+      refreshData("MASK");
+      refreshData("MATIC");
+      refreshData("MAV");
+      refreshData("MDT");
+      refreshData("MINA");
+      refreshData("MKR");
+      refreshData("MTL");
+      refreshData("NEAR");
+      refreshData("NEO");
+      refreshData("NKN");
+      refreshData("NMR");
+      refreshData("OCEAN");
+      refreshData("OGN");
+      refreshData("OMG");
+      refreshData("OM");
+      refreshData("ONE");
+      refreshData("ONT");
+      refreshData("PENDLE");
+      refreshData("PEPE");
+      refreshData("PERP");
+      refreshData("PHB");
+      refreshData("QNT");
+      refreshData("QTUM");
+      refreshData("RAD");
+      refreshData("REEF");
+      refreshData("REN");
+      refreshData("RLC");
+      refreshData("RNDR");
+      refreshData("ROSE");
+      refreshData("RSR");
+      refreshData("RUNE");
+      refreshData("RVN");
+      refreshData("SAND");
+      refreshData("SEI");
+      refreshData("SFP");
+      refreshData("SHIB");
+      refreshData("SKL");
+      refreshData("SNX");
+      refreshData("SNT");
+      refreshData("SOL");
+      refreshData("SPELL");
+      refreshData("SSV");
+      refreshData("STORJ");
+      refreshData("STX");
+      refreshData("SUI");
+      refreshData("SUSHI");
+      refreshData("SXP");
+      refreshData("THETA");
+      refreshData("TLM");
+      refreshData("TOMO");
+      refreshData("TRB");
+      refreshData("TRU");
+      refreshData("TRX");
+      refreshData("T");
+      refreshData("UMA");
+      refreshData("UNFI");
+      refreshData("UNI");
+      refreshData("USDC");
+      refreshData("VET");
+      refreshData("WAVES");
+      refreshData("WLD");
+      refreshData("WOO");
+      refreshData("XEC");
+      refreshData("XEM");
+      refreshData("XLM");
+      refreshData("XMR");
+      refreshData("XRP");
+      refreshData("XTZ");
+      refreshData("XVG");
+      refreshData("XVS");
+      refreshData("YFI");
+      refreshData("YGG");
+      refreshData("ZEC");
+      refreshData("ZEN");
+      refreshData("ZIL");
+      refreshData("ZRX");
     
       // ... (ajoutez d'autres crypto-monnaies à surveiller ici)
     }
