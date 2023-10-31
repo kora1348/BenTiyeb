c1d91c6b4ca3dc5e9230e045e324af23ec353ad1
kora13481 <amelianekor@gmail.com>
2023-10-31 21:43:13 +0100

    ok

--- future/lettre/30min.js
+++ future/lettre/30min.js
@@ -54,20 +54,20 @@ async function fetchCryptoData(symbol) {
     const positiveCount = variations.filter(variation => variation > 0).length;
     const negativeCount = variations.filter(variation => variation < 0).length;
 
-    if (positiveCount >= 6 /*&& totalValue >= 8*/) {
-      totalCell.textContent = "SHORT " + "(" + totalValue + "%)";
-      totalCell.classList.add("negative");
-      const existingContent = cryptoNameDiv.innerHTML;
-      cryptoNameDiv.innerHTML = existingContent
-      ? `${existingContent}<br><span class="negative">${symbol}  ${totalValue} (SHORT)</span>`
-      : `<span class="negative">${symbol} ${totalValue} (SHORT)</span>`;
-    } else if (negativeCount >= 6 /*&& totalValue <= -8*/) {
+    if (positiveCount >= 6 /*&& totalValue >= 4*/) {
       totalCell.textContent = "LONG " + "(" + totalValue + "%)";
       totalCell.classList.add("positive");
       const existingContent = cryptoNameDiv.innerHTML;
       cryptoNameDiv.innerHTML = existingContent
       ? `${existingContent}<br><span class="positive">${symbol}  ${totalValue} (LONG)</span>`
       : `<span class="positive">${symbol} ${totalValue} (LONG)</span>`;
+    } else if (negativeCount >= 6 /*&& totalValue <= -4*/) {
+      totalCell.textContent = "SHORT " + "(" + totalValue + "%)";
+      totalCell.classList.add("negative");
+      const existingContent = cryptoNameDiv.innerHTML;
+      cryptoNameDiv.innerHTML = existingContent
+      ? `${existingContent}<br><span class="negative">${symbol}  ${totalValue} (SHORT)</span>`
+      : `<span class="negative">${symbol} ${totalValue} (SHORT)</span>`;
     } else {
       totalCell.textContent = "/";
       totalCell.classList.add("black");
