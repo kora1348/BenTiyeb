30faaaef127048665b3f851c1be69933fb0a2216
kora13481 <amelianekor@gmail.com>
2023-11-13 00:14:13 +0100

    toto

--- future/lettre/salma.js
+++ future/lettre/salma.js
@@ -1,59 +1,65 @@
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=19`
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=4h&limit=16`
     );
     const data = await response.json();
 
-    // Calcul du total des taux de variation sur 4 intervalles de 1 minute (en excluant l'intervalle courant)
+    // Calcul du total des taux de variation sur 4 intervalles de 15 minutes
     let totalVariation = 0;
 
     // Mise à jour du tableau avec les données et la couleur
     const cryptoRow = document.getElementById(symbol);
 
-    const variations = [];
-
-    for (let i = 0; i < data.length - 1; i++) {
+    for (let i = 0; i < data.length; i++) {
       const openPrice = parseFloat(data[i][1]);
       const closePrice = parseFloat(data[i][4]);
       const intervalVariation = ((closePrice - openPrice) / openPrice) * 100;
-      const cellIndex = i + 1;
+      const cellIndex = i + 1; // Décalage d'une cellule pour éviter la première cellule (Crypto)
 
       const variationCell = cryptoRow.insertCell(cellIndex);
       const variationValue = intervalVariation.toFixed(2);
-
-      // Afficher l'heure de l'intervalle et la variation
       const timestamp = parseInt(data[i][0]);
       const dateValue = new Date(timestamp);
-      dateValue.setMinutes(dateValue.getMinutes() + 5);
-
       const hour = dateValue.getHours();
       const minute = dateValue.getMinutes();
-      const formattedTime = `${hour.toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}`;
+      const formattedTime = `${hour.toString().padStart(2, "0")}:${minute
+        .toString()
+        .padStart(2, "0")}`;
+
       variationCell.textContent = `${formattedTime}: ${variationValue}%`;
 
-      if (intervalVariation > 0) {
-        variationCell.classList.add("positive");
-      } else if (intervalVariation < 0) {
-        variationCell.classList.add("negative");
-      }
-
-      totalVariation += intervalVariation;
-      variations.push(intervalVariation);
+      // Ne pas ajouter de classe de couleur aux cellules individuelles
+      totalVariation += intervalVariation; // Ajouter la variation de l'intervalle au total
     }
 
     // Ajouter la cellule pour afficher le total de variation
-    const totalCell = cryptoRow.insertCell(data.length);
+    const totalCell = cryptoRow.insertCell(data.length + 1);
     const totalValue = totalVariation.toFixed(2);
     totalCell.textContent = `${totalValue}%`;
 
-    const cryptoNameDiv = document.getElementById("cryptoNames");
+    if (totalVariation >= 0.80 && totalVariation <= 0.90) {
+      totalCell.classList.add("negative");
+    } else if (totalVariation <= -0.80 && totalVariation >= -0.90) {
+      totalCell.classList.add("positive");
+    }
 
-    // Vérifier si au moins deux des trois variations sont positives et afficher "VERT" dans la cellule totalCell
-    const positiveCount = variations.filter(variation => variation > 0).length;
-    const negativeCount = variations.filter(variation => variation < 0).length;
+    if ( (totalVariation >= 0.80 && totalVariation <= 0.90) ||  (totalVariation <= -0.80 && totalVariation >= -0.90)) {
+      // Ajouter le nom de la crypto en dehors du tableau
+      const cryptoNameDiv = document.getElementById("cryptoNames");
+      const cryptoName = document.createElement("p");
+      cryptoName.textContent = `${symbol} : ${totalValue}%`;
+      cryptoNameDiv.appendChild(cryptoName);
 
-   
+      // Ajouter la classe CSS appropriée
+      if (totalVariation >= 0.80 && totalVariation <= 0.90) {
+        cryptoName.classList.add("negative");
+      } else if (totalVariation <= -0.80 && totalVariation >= -0.90) {
+        cryptoName.classList.add("positive");
+      }
+    }
+
+
   } catch (error) {
     console.error(
       `Erreur lors de la récupération des données pour ${symbol}:`,
@@ -61,6 +67,7 @@ async function fetchCryptoData(symbol) {
     );
   }
 }
+
 
 
   // Appel de la fonction pour obtenir les taux de variation des cryptos
