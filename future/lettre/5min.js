7b6efb5ac373711526fdd40f43705a55964bbcea
kora13481 <amelianekor@gmail.com>
2023-11-18 15:01:22 +0100

    totot

--- future/lettre/5min.js
+++ future/lettre/5min.js
@@ -51,7 +51,7 @@ function displayNegativeTotals(cryptoData) {
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=15m&limit=33`
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=33`
     );
     const data = await response.json();
 
