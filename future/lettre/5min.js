e8d385751363a519927c3eae961afc6b115e8fc1
kora13481 <amelianekor@gmail.com>
2023-11-18 14:10:22 +0100

    toto

--- future/lettre/5min.js
+++ future/lettre/5min.js
@@ -7,6 +7,8 @@ async function fetchCryptoData(symbol) {
 
     // Calcul du total des taux de variation sur 4 intervalles de 15 minutes
     let totalVariation = 0;
+    let positiveCount = 0;
+    let negativeCount = 0;
 
     // Mise à jour du tableau avec les données et la couleur
     const cryptoRow = document.getElementById(symbol);
@@ -31,9 +33,24 @@ async function fetchCryptoData(symbol) {
 
       // Ne pas ajouter de classe de couleur aux cellules individuelles
       totalVariation += intervalVariation; // Ajouter la variation de l'intervalle au total
+
+      // Compter les variations positives et négatives
+      if (intervalVariation > 0) {
+        positiveCount++;
+      } else if (intervalVariation < 0) {
+        negativeCount++;
+      }
     }
 
-   
+    // Afficher le total, le nombre de variations positives et le nombre de variations négatives
+    const totalCell = cryptoRow.insertCell(data.length + 1);
+    totalCell.textContent = `Total: ${totalVariation.toFixed(2)}%`;
+
+    const positiveCell = cryptoRow.insertCell(data.length + 2);
+    positiveCell.textContent = `Positif: ${positiveCount}`;
+
+    const negativeCell = cryptoRow.insertCell(data.length + 3);
+    negativeCell.textContent = `Negatif: ${negativeCount}`;
   } catch (error) {
     console.error(
       `Erreur lors de la récupération des données pour ${symbol}:`,
@@ -42,6 +59,7 @@ async function fetchCryptoData(symbol) {
   }
 }
 
+// Appeler la fonction pour chaque crypto-monnaie
 fetchCryptoData("1INCH");
 fetchCryptoData("AAVE");
 fetchCryptoData("ACH");
