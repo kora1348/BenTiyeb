d01c499bbfd502be5df911cc4b6c1ed1b733d977
kora13481 <amelianekor@gmail.com>
2023-11-18 14:25:04 +0100

    toto

--- future/lettre/5min.js
+++ future/lettre/5min.js
@@ -1,3 +1,28 @@
+// Fonction pour afficher les totaux positifs supérieurs ou égaux à 2 dans la balise avec l'ID "messagePositive"
+function displayPositiveTotals(cryptoData) {
+  const messagePositive = document.getElementById("messagePositive");
+  messagePositive.innerHTML = ""; // Effacer le contenu précédent
+
+  cryptoData.forEach((crypto) => {
+    const symbol = crypto.symbol;
+    const totalVariation = crypto.totalVariation;
+    const positiveCount = crypto.positiveCount;
+    const negativeCount = crypto.negativeCount;
+
+    // Afficher uniquement si le total positif est supérieur ou égal à 2
+    if (positiveCount >= 2) {
+      // Ajouter des classes de couleur au total
+      const totalText = `Total pour ${symbol}: ${totalVariation.toFixed(2)}% (Positif: ${positiveCount}, Negatif: ${negativeCount})`;
+      const totalElement = document.createElement("p");
+      totalElement.textContent = totalText;
+      totalElement.style.color =
+        totalVariation > 0 ? "green" : totalVariation < 0 ? "red" : "black";
+
+      messagePositive.appendChild(totalElement);
+    }
+  });
+}
+
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
@@ -33,43 +58,57 @@ async function fetchCryptoData(symbol) {
 
       // Ajouter des classes de couleur aux cellules individuelles
       if (intervalVariation > 0) {
-        variationCell.style.color = 'green';
+        variationCell.style.color = "green";
         positiveCount++;
       } else if (intervalVariation < 0) {
-        variationCell.style.color = 'red';
+        variationCell.style.color = "red";
         negativeCount++;
       }
-      
+
       totalVariation += intervalVariation; // Ajouter la variation de l'intervalle au total
     }
 
     // Ajouter des classes de couleur au total
     const totalCell = cryptoRow.insertCell(data.length + 1);
     totalCell.textContent = `Total: ${totalVariation.toFixed(2)}%`;
-    totalCell.style.color = totalVariation > 0 ? 'green' : totalVariation < 0 ? 'red' : 'black';
+    totalCell.style.color =
+      totalVariation > 0 ? "green" : totalVariation < 0 ? "red" : "black";
 
     const positiveCell = cryptoRow.insertCell(data.length + 2);
     positiveCell.textContent = `Positif: ${positiveCount}`;
-    positiveCell.style.color = 'green';
+    positiveCell.style.color = "green";
 
     const negativeCell = cryptoRow.insertCell(data.length + 3);
     negativeCell.textContent = `Negatif: ${negativeCount}`;
-    negativeCell.style.color = 'red';
+    negativeCell.style.color = "red";
+
+    // Retourner les données pour l'affichage des totaux
+    return {
+      symbol,
+      totalVariation,
+      positiveCount,
+      negativeCount,
+    };
   } catch (error) {
-    console.error(
-      `Erreur lors de la récupération des données pour ${symbol}:`,
-      error
-    );
+    console.error(`Erreur lors de la récupération des données pour ${symbol}:`, error);
   }
 }
 
 // Appeler la fonction pour chaque crypto-monnaie
-fetchCryptoData("1INCH");
-fetchCryptoData("AAVE");
-fetchCryptoData("ACH");
-fetchCryptoData("ADA");
-fetchCryptoData("AGIX");
-fetchCryptoData("AGLD");
-fetchCryptoData("ALGO");
-fetchCryptoData("ALICE");
-fetchCryptoData("ALPHA");
+const cryptoDataPromises = [
+  fetchCryptoData("1INCH"),
+  fetchCryptoData("AAVE"),
+  fetchCryptoData("ACH"),
+  fetchCryptoData("ADA"),
+  fetchCryptoData("AGIX"),
+  fetchCryptoData("AGLD"),
+  fetchCryptoData("ALGO"),
+  fetchCryptoData("ALICE"),
+  fetchCryptoData("ALPHA"),
+];
+
+// Attendre que toutes les promesses soient résolues
+Promise.all(cryptoDataPromises).then((cryptoDataArray) => {
+  // Afficher les totaux positifs
+  displayPositiveTotals(cryptoDataArray);
+});
