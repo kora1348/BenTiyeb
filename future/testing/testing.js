74e96a003c08f4431cd937dc6f4d2fe32be5645c
kora13481 <amelianekor@gmail.com>
2023-11-28 12:34:34 +0100

    otot

--- future/testing/testing.js
+++ future/testing/testing.js
@@ -63,7 +63,7 @@ function calculateAndDisplayTotal(cryptoData) {
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=1`
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=33`
     );
     const data = await response.json();
 
