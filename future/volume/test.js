92b8ac6e6327a7e7b736e57b075133a0c0204996
kora13481 <amelianekor@gmail.com>
2023-12-02 16:37:31 +0100

    toto

--- future/volume/test.js
+++ future/volume/test.js
@@ -1,19 +1,29 @@
 async function fetchCryptoData(symbol) {
   try {
-    const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=8`
-    );
-    
-   
-    
+      const response = await fetch(
+          `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=8`
+      );
+
+      const data = await response.json();
+
+      // Calculez le taux de variation pour chaque intervalle
+      for (let i = 0; i < data.length; i++) {
+          const openPrice = parseFloat(data[i][1]);
+          const closePrice = parseFloat(data[i][4]);
+          const variation = ((closePrice - openPrice) / openPrice) * 100;
+          
+          // Mettez à jour le contenu HTML avec les taux de variation
+          const element = document.getElementById(`variation_${symbol}_${i + 1}`);
+          element.textContent = variation.toFixed(2) + "%";
+      }
+
   } catch (error) {
-    console.error(
-      `Erreur lors de la récupération des données pour ${symbol}:`,
-      error
-    );
+      console.error(
+          `Erreur lors de la récupération des données pour ${symbol}:`,
+          error
+      );
   }
 }
 
-  // Appel de la fonction pour obtenir les taux de variation des cryptos
-  fetchCryptoData("1INCH");
-  
+// Appel de la fonction pour obtenir les taux de variation des cryptos
+fetchCryptoData("1INCH");
