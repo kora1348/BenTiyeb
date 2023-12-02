229e9ef695ead7b8aab93d0fa0667a232b3a915e
kora13481 <amelianekor@gmail.com>
2023-12-02 22:42:17 +0100

    toto

--- future/volume/copie.js
+++ future/volume/copie.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=2`
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=3`
     );
 
     const data = await response.json();
@@ -10,29 +10,30 @@ async function fetchCryptoData(symbol) {
     let totalVariation = 0;
     const variations = [];
 
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 1; i < data.length; i++) {
       const openPrice = parseFloat(data[i][1]);
       const closePrice = parseFloat(data[i][4]);
       const variation = ((closePrice - openPrice) / openPrice) * 100;
       const time = new Date(data[i][0]).toLocaleTimeString('fr-FR', { hour: 'numeric', minute: 'numeric', hour12: false });
-
+    
       // Mettez à jour le contenu HTML avec les taux de variation, l'heure et la couleur
-      const element = document.getElementById(`variation_${symbol}_${i + 1}`);
+      const element = document.getElementById(`variation_${symbol}_${i}`);
       element.textContent = `${variation.toFixed(2)}% ${time}`;
-
+    
       // Ajoutez la classe de couleur en fonction de la positivité ou de la négativité
       if (variation > 0) {
         element.classList.add("positive");
       } else if (variation < 0) {
         element.classList.add("negative");
       }
-
+    
       // Calculez le total des taux de variation
       totalVariation += variation;
-
+    
       // Stockez les variations dans le tableau
       variations.push(variation);
     }
+    
 
     // Mettez à jour le contenu HTML avec le total et appliquez la classe de couleur bleue
     const totalElement = document.getElementById(`total_${symbol}`);
@@ -65,8 +66,6 @@ async function fetchCryptoData(symbol) {
     );
   }
 }
-
-
 // Appel de la fonction pour obtenir les taux de variation des cryptos
 fetchCryptoData("1INCH");
   fetchCryptoData("AAVE");
