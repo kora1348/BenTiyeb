6f346907f2eb0b0464adca39d8cb72944a6c5df6
kora13481~ <amelianekor@gmail.com>
2023-12-25 11:58:11 +0100

    tttr

--- future/variation/var.js
+++ future/variation/var.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
 	try {
 		const response = await fetch(
-			`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1m&limit=6`
+			`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=6`
 		);
 
 		const data = await response.json();
