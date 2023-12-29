9ad0806ee2143b24ef14d2011fa404b0d88a745f
kora13481~ <amelianekor@gmail.com>
2023-12-29 23:44:13 +0100

    oko

--- future/mois/3mois.js
+++ future/mois/3mois.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
   try {
       const response = await fetch(
-          `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1M&limit=1`
+          `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1M&limit=3`
       );
       const data = await response.json();
 
@@ -47,9 +47,9 @@ async function fetchCryptoData(symbol) {
       const cryptoNamesElement = document.getElementById('cryptoNames');
 
       // Ajouter la classe "positive" pour le total dans la plage spécifiée
-      if (totalVariation >= 70 && totalVariation <= 79) {
-          totalCell.classList.add("negative");
-          cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="negative">${symbol}: SHORT, ${totalValue}%</p>`;
+      if (totalVariation >= -108 && totalVariation <= -90) {
+          totalCell.classList.add("positive");
+          cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="positive">${symbol}: LONG, ${totalValue}%</p>`;
       }
 
       totalCell.textContent = `${totalValue}%`;
