f955bf6a369214d009d5f764abd007b5f18b4f21
kora13481~ <amelianekor@gmail.com>
2023-12-30 22:27:56 +0100

    toto

--- future/mois/day.js
+++ future/mois/day.js
@@ -4,62 +4,62 @@ async function fetchCryptoData(symbol) {
             `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1d&limit=3`
         );
         const data = await response.json();
-  
+
         // Calcul du total des taux de variation sur 3 semaines
         let totalVariation = 0;
-  
+
         // Mise à jour du tableau avec les données et la couleur
         const cryptoRow = document.getElementById(symbol);
-  
+
         for (let i = 0; i < data.length; i++) {
             const openPrice = parseFloat(data[i][1]);
             const closePrice = parseFloat(data[i][4]);
+            const volume = parseFloat(data[i][5]);
             const weeklyVariation = ((closePrice - openPrice) / openPrice) * 100;
             const cellIndex = i + 1; // Décalage d'une cellule pour éviter la première cellule (Crypto)
-  
+
             const variationCell = cryptoRow.insertCell(cellIndex);
             const variationValue = weeklyVariation.toFixed(2);
-  
+
             // Formatage de la date de début de semaine
             const weekStartDate = new Date(data[i][0]);
             const optionsStart = { year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "numeric" };
             const formattedStartDate = weekStartDate.toLocaleDateString("fr-FR", optionsStart);
-  
-            // Suppression des dates et heures
-            variationCell.textContent = `${formattedStartDate} : ${variationValue}%`;
-  
+
+            // Ajouter les informations de prix et volume
+            variationCell.textContent = `${formattedStartDate}: Variation ${variationValue}%, Open: ${openPrice}, Close: ${closePrice}, Volume: ${volume}`;
+
             // Ajouter la classe "positive" ou "negative" en fonction de la variation hebdomadaire
             if (weeklyVariation > 0) {
                 variationCell.classList.add("positive");
             } else if (weeklyVariation < 0) {
                 variationCell.classList.add("negative");
             }
-  
+
             totalVariation += weeklyVariation; // Ajouter la variation hebdomadaire au total
         }
-  
+
         // Ajouter la cellule pour afficher le total de variation
         const totalCell = cryptoRow.insertCell(data.length + 1);
         const totalValue = totalVariation.toFixed(2);
-  
+
         const cryptoNamesElement = document.getElementById('cryptoNames');
-  
+
         // Ajouter la classe "positive" pour le total dans la plage spécifiée
         if (totalVariation >= -108 && totalVariation <= -90) {
             totalCell.classList.add("positive");
             cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="positive">${symbol}: LONG, ${totalValue}%</p>`;
         }
-  
+
         totalCell.textContent = `${totalValue}%`;
-  
+
     } catch (error) {
         console.error(
             `Erreur lors de la récupération des données pour ${symbol}:`,
             error
         );
     }
-  }
-  
+}
 
   
   // Appel de la fonction pour obtenir les taux de variation des cryptos
