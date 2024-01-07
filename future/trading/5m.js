266c0cfb07e385b03f5b0a6f2645517dc1b01b4f
kora13481~ <amelianekor@gmail.com>
2024-01-07 19:26:38 +0100

    totot

--- future/trading/5m.js
+++ future/trading/5m.js
@@ -1,19 +1,16 @@
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=16`
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=1`
     );
     const data = await response.json();
+    
+          // Calcul du total des taux de variation sur 3 semaines
+          let totalVariation = 0;
 
     const cryptoRow = document.getElementById(symbol);
 
-    let isShortSignal = data.every(
-      (candle) => parseFloat(candle[4]) - parseFloat(candle[1]) > 0
-    );
 
-    let isLongSignal = data.every(
-      (candle) => parseFloat(candle[4]) - parseFloat(candle[1]) < 0
-    );
 
     for (let i = 0; i < data.length; i++) {
       const openPrice = parseFloat(data[i][1]);
@@ -44,29 +41,43 @@ async function fetchCryptoData(symbol) {
       } else if (weeklyVariation < 0) {
         variationCell.classList.add("negative");
       }
+
+      totalVariation += weeklyVariation; // Ajouter la variation hebdomadaire au total
+
     }
 
+          // Ajouter la cellule pour afficher le total de variation
+          const totalCell = cryptoRow.insertCell(data.length + 1);
+          const totalValue = totalVariation.toFixed(2);
     const achatCell = cryptoRow.insertCell(data.length + 1);
     const venteCell = cryptoRow.insertCell(data.length + 2);
     const cryptoNamesElement = document.getElementById("cryptoNames");
 
-    if (isShortSignal) {
+     // Ajouter la classe "positive" pour le total dans la plage spécifiée
+     if (totalVariation >= 1) {
+      totalCell.classList.add("negative");
       venteCell.textContent = "SHORT";
       venteCell.classList.add("negative");
-      cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="negative">${symbol}: SHORT, </p>`;
+      cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="negative">${symbol}: SHORT, ${totalValue}%</p>`;
       showNotification(`${symbol}: Signal SHORT - 5m`);
-    } else {
-      venteCell.textContent = "-";
-    }
+  }else {
+    venteCell.textContent = "-";
+  }
 
-    if (isLongSignal) {
-      achatCell.textContent = "LONG";
-      achatCell.classList.add("positive");
-      cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="positive">${symbol}: LONG, </p>`;
-      showNotification(`${symbol}: Signal LONG - 5m`);
-    } else {
-      achatCell.textContent = "-";
-    }
+  if (totalVariation <= -1) {
+    totalCell.classList.add("positive");
+    achatCell.textContent = "LONG";
+    achatCell.classList.add("positive");
+    cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="positive">${symbol}: LONG, ${totalValue}%</p>`;
+    showNotification(`${symbol}: Signal LONG - 5m`);
+}else {
+  achatCell.textContent = "-";
+}
+
+
+
+
+    totalCell.textContent = `${totalValue}%`;
 
   } catch (error) {
     console.error(
