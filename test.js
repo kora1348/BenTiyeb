303eb5a592a9b82fdccb5a23c11671c907583e10
kora13481~ <amelianekor@gmail.com>
2024-02-06 20:25:00 +0100

    toto

--- test.js
+++ test.js
@@ -1,20 +1,12 @@
-async function fetchCryptoData(symbol) {
-  try {
-      // Récupérer des informations détaillées sur une paire spécifique
-      const informationsPaireResponse = await fetch(`https://api.binance.com/api/v3/ticker/24hr?symbol=${symbol}`);
-      const informationsPaireData = await informationsPaireResponse.json();
-
-      console.log('Informations détaillées sur la paire', symbol, ':', informationsPaireData);
-
-      // Faites ce que vous avez besoin de faire avec les données récupérées ici
-      
-  } catch (error) {
-      console.error(
-          `Erreur lors de la récupération des données pour ${symbol}:`,
-          error
-      );
-  }
-}
-
-// Exemple d'utilisation avec une paire spécifique (remplacez par la paire que vous souhaitez)
-fetchCryptoData('BTCUSDT');
+// Utilisation de l'API fetch pour effectuer une requête HTTP
+fetch('https://api.coingecko.com/api/v3/coins/list')
+  .then(response => response.json()) // Convertir la réponse en JSON
+  .then(data => {
+    // Parcourir les données pour afficher chaque crypto et sa date de listing
+    data.forEach(crypto => {
+      console.log(`Nom : ${crypto.name}, Symbole : ${crypto.symbol}, Date de listing : ${crypto.ath_date}`);
+    });
+  })
+  .catch(error => {
+    console.error('Une erreur s\'est produite :', error);
+  });
