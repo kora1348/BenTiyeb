7d3bab5f044de739a6da133abe183f0d78ede63d
kora13481~ <amelianekor@gmail.com>
2024-02-21 14:17:45 +0100

    toto

--- ta.js
+++ ta.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
   try {
       const response = await fetch(
-          `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=7`
+          `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1d&limit=7`
       );
       const data = await response.json();
 
@@ -50,7 +50,7 @@ async function fetchCryptoData(symbol) {
       const cryptoNamesElement = document.getElementById('cryptoNames');
 
       // Ajouter la classe "positive" pour le total dans la plage spécifiée
-      if (totalVariation >= -3.99 && totalVariation <= -3.00) {
+      if (totalVariation <= -16.00) {
           totalCell.classList.add("positive");
           cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="positive">${symbol}: LONG, ${totalValue}%</p>`;
       }
