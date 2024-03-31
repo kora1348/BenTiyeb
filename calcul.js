58c2be3c60143041cb9bd958b62eb35ac33d5859
kora1348 <amelianekor@gmail.com>
2024-03-31 15:14:44 +0200

    toto

--- calcul.js
+++ calcul.js
@@ -1,6 +1,6 @@
-function calculate() {
-    var input = document.getElementById("decimalInput").value;
-    var prixTmp = (parseFloat(input) * 2) / 100; 
-    var result = input - prixTmp;
+function calculerPrixCrypto() {
+    var prixEntree = document.getElementById("decimalInput").value;
+    var prixTmp = (parseFloat(prixEntree) * 2) / 100; 
+    var result = prixEntree - prixTmp;
     document.getElementById("result").innerText = "Résultat : " + result.toFixed(10);
 }
