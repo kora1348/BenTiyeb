09a08dcd2873c4c1ff2566bd9b3bedd5a139e3e1
kora1348 <amelianekor@gmail.com>
2024-04-14 19:01:20 +0200

    toto

--- cal.js
+++ cal.js
@@ -6,35 +6,35 @@ function calculerPrixCrypto() {
     var prixApresReductionQuatrePourcent = prixEntree - reductionQuatrePourcent;
     
     // Affichage du résultat après réduction de 4%
-    document.getElementById("result1").innerText = "Résultat après -4% : " + prixApresReductionQuatrePourcent.toFixed(10);
+    document.getElementById("result1").innerText = "Résultat après -4% : " + prixApresReductionQuatrePourcent.toFixed(10) + "  => 03";
     
     // Calcul de la réduction supplémentaire de 8% sur le prix d'entrée initial
     var reductionHuitPourcent = (parseFloat(prixEntree) * 8) / 100;
     var resultatFinal = prixEntree - reductionHuitPourcent;
 
     // Affichage du résultat après réduction de 8%
-    document.getElementById("result2").innerText = "Résultat après -8% : " + resultatFinal.toFixed(10);
+    document.getElementById("result2").innerText = "Résultat après -8% : " + resultatFinal.toFixed(10) + "  => 06";
 
     // Calcul de la réduction supplémentaire de 12% sur le prix d'entrée initial
     var reductionDouzePourcent = (parseFloat(prixEntree) * 12) / 100;
     var resultatFinal = prixEntree - reductionDouzePourcent;
 
     // Affichage du résultat après réduction de 12%
-    document.getElementById("result3").innerText = "Résultat après -12% : " + resultatFinal.toFixed(10);
+    document.getElementById("result3").innerText = "Résultat après -12% : " + resultatFinal.toFixed(10) + "  => 09";
 
      // Calcul de la réduction supplémentaire de 16% sur le prix d'entrée initial
      var reductionSeizePourcent = (parseFloat(prixEntree) * 16) / 100;
      var resultatFinal = prixEntree - reductionSeizePourcent;
  
      // Affichage du résultat après réduction de 16%
-     document.getElementById("result4").innerText = "Résultat après -16% : " + resultatFinal.toFixed(10);
+     document.getElementById("result4").innerText = "Résultat après -16% : " + resultatFinal.toFixed(10) + "  => 18";
 
      // Calcul de la réduction supplémentaire de 20% sur le prix d'entrée initial
     var reductionVingtPourcent = (parseFloat(prixEntree) * 20) / 100;
     var resultatVingtPourcent = prixEntree - reductionVingtPourcent;
     
     // Affichage du résultat après réduction de 20%
-    document.getElementById("result5").innerText = "Résultat après -20% : " + resultatVingtPourcent.toFixed(10);
+    document.getElementById("result5").innerText = "Résultat après -20% : " + resultatVingtPourcent.toFixed(10)  + "  => 36";
     
     // Calcul de la réduction supplémentaire de 24% sur le prix d'entrée initial
     var reductionVingtQuatrePourcent = (parseFloat(prixEntree) * 24) / 100;
