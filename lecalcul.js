01971893f820105e8e9ec4ba906507aa648e1194
kora1348 <amelianekor@gmail.com>
2024-04-19 22:37:38 +0200

    toto

--- lecalcul.js
+++ lecalcul.js
@@ -17,10 +17,29 @@ function calculerPrixCrypto() {
 
     // Calcul de la réduction supplémentaire de 1.50% sur le prix d'entrée initial
     var reductionUnCinquantePourcent = (parseFloat(prixEntree) * 1.50) / 100;
-    var resultatFinal = prixEntree - reductionUnCinquantePourcent;
+    resultatFinal = prixEntree - reductionUnCinquantePourcent;
 
     // Affichage du résultat après réduction de 1.50%
     document.getElementById("result3").innerText = "Résultat après -1.50% : " + resultatFinal.toFixed(10);
 
-     
+    // Calcul de la réduction de 0.50% avant le prix d'entrée initial
+    var reductionZeroCinquantePourcentAvant = (parseFloat(prixEntree) * 0.50) / 100; 
+    var prixAvantReductionZeroCinquantePourcent = parseFloat(prixEntree) + reductionZeroCinquantePourcentAvant;
+    
+    // Affichage du résultat avant réduction de 0.50%
+    document.getElementById("result4").innerText = "Résultat avant -0.50% : " + prixAvantReductionZeroCinquantePourcent.toFixed(10);
+    
+    // Calcul de la réduction supplémentaire de 1% avant le prix d'entrée initial
+    var reductionUnPourcentAvant = (parseFloat(prixEntree) * 1) / 100;
+    var prixAvantReductionUnPourcent = parseFloat(prixEntree) + reductionUnPourcentAvant;
+
+    // Affichage du résultat avant réduction de 1%
+    document.getElementById("result5").innerText = "Résultat avant -1% : " + prixAvantReductionUnPourcent.toFixed(10);
+
+    // Calcul de la réduction supplémentaire de 1.50% avant le prix d'entrée initial
+    var reductionUnCinquantePourcentAvant = (parseFloat(prixEntree) * 1.50) / 100;
+    var prixAvantReductionUnCinquantePourcent = parseFloat(prixEntree) + reductionUnCinquantePourcentAvant;
+
+    // Affichage du résultat avant réduction de 1.50%
+    document.getElementById("result6").innerText = "Résultat avant -1.50% : " + prixAvantReductionUnCinquantePourcent.toFixed(10);
 }
