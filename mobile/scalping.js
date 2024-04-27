06b6bbe480ad6a992e723499952812131968891a
kora1348 <amelianekor@gmail.com>
2024-04-27 18:29:23 +0200

    toto

--- mobile/scalping.js
+++ mobile/scalping.js
@@ -5,20 +5,17 @@ async function fetchCryptoData(symbol) {
         );
         const data = await response.json();
   
-        // Calcul du total des taux de variation sur 3 semaines
-        let totalVariation = 0;
-  
         // Mise à jour du tableau avec les données et la couleur
         const cryptoRow = document.getElementById(symbol);
   
         for (let i = 0; i < data.length; i++) {
             const openPrice = parseFloat(data[i][1]);
             const closePrice = parseFloat(data[i][4]);
-            const weeklyVariation = ((closePrice - openPrice) / openPrice) * 100;
+            const variationColor = ((closePrice - openPrice) / openPrice) * 100;
             const cellIndex = i + 1; // Décalage d'une cellule pour éviter la première cellule (Crypto)
   
             const variationCell = cryptoRow.insertCell(cellIndex);
-            const variationValue = weeklyVariation.toFixed(2);
+            const variationValue = variationColor.toFixed(2);
             const weekStartDate = new Date(data[i][0]);
             const weekEndDate = new Date(data[i][6]);
             const optionsStart = { year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "numeric" };
@@ -31,31 +28,21 @@ async function fetchCryptoData(symbol) {
                 optionsStart
             )} (${weekEndDate.toLocaleTimeString("fr-FR", optionsEnd)}): ${variationValue}%`;
   
-            // Ajouter la classe "positive" ou "negative" en fonction de la variation hebdomadaire
-            if (weeklyVariation > 0) {
+            if (variationColor > 0) {
                 variationCell.classList.add("positive");
-            } else if (weeklyVariation < 0) {
+            } else if (variationColor < 0) {
                 variationCell.classList.add("negative");
             }
   
-            totalVariation += weeklyVariation; // Ajouter la variation hebdomadaire au total
         }
   
-        // Ajouter la cellule pour afficher le total de variation
-        const totalCell = cryptoRow.insertCell(data.length + 1);
-        const totalValue = totalVariation.toFixed(2);
-        
-        totalCell.style.textAlign = 'center';
   
         const cryptoNamesElement = document.getElementById('cryptoNames');
   
-        // Ajouter la classe "positive" pour le total dans la plage spécifiée
-        if (totalVariation >= -4.99 && totalVariation <= -1.00) {
-            totalCell.classList.add("negative");
-            cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="negative">${symbol}: SHORT, ${totalValue}%</p>`;
-        }
-    
-        totalCell.textContent = `${totalValue}%`;
+
+  
+        
+
   
     } catch (error) {
         console.error(
@@ -64,7 +51,8 @@ async function fetchCryptoData(symbol) {
         );
     }
   }
-  
+
+
   
   function mettreAJourHeure() {
       var elementHeure = document.getElementById('heure');
