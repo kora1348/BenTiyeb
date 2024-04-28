52f38d0b0237e01091553521009dc85e68b09a15
kora1348 <amelianekor@gmail.com>
2024-04-28 23:07:46 +0200

    toto

--- mobile/15m.js
+++ mobile/15m.js
@@ -1,6 +1,6 @@
 async function fetchAndDisplayCryptoData(symbol, limit) {
     try {
-        const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1m&limit=${limit}`);
+        const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=15m&limit=${limit}`);
         const data = await response.json();
 
         let totalVariation = 0;
@@ -38,7 +38,7 @@ async function fetchAndDisplayCryptoData(symbol, limit) {
         const cryptoNamesElement = document.getElementById('cryptoNames');
 
         if (limit === 9) {
-            const response21 = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1m&limit=21`);
+            const response21 = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=15m&limit=21`);
             const data21 = await response21.json();
             let totalVariation21 = 0;
             for (let i = 0; i < data21.length; i++) {
@@ -90,7 +90,7 @@ function mettreAJourHeure() {
 async function fetchAndDisplayAllCryptoData() {
     mettreAJourHeure();
 
-    const symbols = ["1INCH", "AAVE", "ACE", "ACH", "ADA", "AEVO", /* and so on... */];
+    const symbols = ["1INCH", "AAVE", "ACE", "ACH", "ADA", "AEVO", "AGIX","AGLD","ALGO","ALICE","ALPHA","ALT","AMB","ANKR","ANT","APE","API3","APT","AR","ARB","ARK","ARKM","ARPA","ASTR","ATA","ATOM","AUCTION","AUDIO","AVAX","AXL","AXS","BADGER","BAKE","BAL","BAND","BAT","BCH","BEAMX","BEL","BICO","BLUR","BLZ","BNB","BNT","BNX","BOME","BOND","BONK","BTC","C98","CAKE","CELO","CELR","CFX","CHR","CHZ","COMBO","COMP","COTI","CRV","CTK","CTSI","CVX","CYBER","DAR","DASH","DENT","DGB","DOGE","DOT","DUSK","DYM","DYDX","EDU","EGLD","ENJ","ENS","EOS","ETC","ETH","ETHFI","FET","FIL","FLM","FLOKI","FLOW","FRONT","FTM","FXS","GALA","GAL","GAS","GLMR","GMT","GMX","GRT","GTC","HBAR","HFT","HIFI","HIGH","HOOK","HOT","ICP","ICX","IDEX","ID","ILV","IMX","INJ","IOST","IOTA","IOTX","JASMY","JOE","JTO","JUP","KAVA","KEY","KLAY","KNC","KSM","LDO","LEVER","LINA","LINK","LIT","LOOM","LPT","LQTY","LRC","LSK","LTC","LUNC","MAGIC","MANTA","MANA","MASK","MATIC","MAV","MBL","MDT","MEME","METIS","MINA","MKR","MOVR","MTL","NEAR","NEO","NFP","NKN","NMR","NTRN","OCEAN","OGN","OMG","ONE","ONDO","ONG","ONT","OP","ORBS","ORDI","OXT","PENDLE","PEOPLE","PEPE","PERP","PHB","PIXEL","POLYX","PORTAL","POWR","PYTH","QNT","QTUM","RAD","RDNT","REEF","REN","RLC","RNDR","RONIN","ROSE","RSR","RUNE","RVN","SAND","1000SATS","SEI","SFP","SHIB","SKL","SLP","SNT","SNX","SOL","SPELL","SSV","STEEM","STG","STMX","STORJ","STPT","STRAX","STRK","STX","SUI","SUPER","SUSHI","SXP","THETA","TIA","TLM","TRB","TRU","TRX","T","TWT","UMA","UNFI","UNI","USDC","USTC","USDT","VET","WAVES","WAXP","WIF","WLD","WOO","XEM","XLM","XRP","XTZ","XVG","XVS","YFI","YGG","ZEC","ZEN","ZIL","ZRX"];
 
     for (const symbol of symbols) {
         await fetchAndDisplayCryptoData(symbol, 9);
