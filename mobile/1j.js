4e8f3b8475039ec6e0698f5dcb0fc7bf72a76b96
kora1348 <amelianekor@gmail.com>
2024-05-16 17:03:34 +0200

    toto

---
+++ mobile/1j.js
@@ -0,0 +1,100 @@
+async function fetchAndDisplayCryptoData(symbol, limit) {
+    try {
+        const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1d&limit=${limit}`);
+        const data = await response.json();
+
+        let totalVariation = 0;
+
+        const cryptoRow = document.getElementById(symbol);
+
+        for (let i = 0; i < data.length; i++) {
+            const openPrice = parseFloat(data[i][1]);
+            const closePrice = parseFloat(data[i][4]);
+            const weeklyVariation = ((closePrice - openPrice) / openPrice) * 100;
+            const cellIndex = i + 1;
+
+            const variationCell = cryptoRow.insertCell(cellIndex);
+            const variationValue = weeklyVariation.toFixed(2);
+            const weekStartDate = new Date(data[i][0]);
+            const weekEndDate = new Date(data[i][6]);
+            const optionsStart = { year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "numeric" };
+            const optionsEnd = { hour: "numeric", minute: "numeric" };
+            variationCell.textContent = `${weekStartDate.toLocaleDateString("fr-FR", optionsStart)} (${weekStartDate.toLocaleTimeString("fr-FR", optionsEnd)}) - ${weekEndDate.toLocaleDateString("fr-FR", optionsStart)} (${weekEndDate.toLocaleTimeString("fr-FR", optionsEnd)}): ${variationValue}%`;
+
+            if (weeklyVariation > 0) {
+                variationCell.classList.add("positive");
+            } else if (weeklyVariation < 0) {
+                variationCell.classList.add("negative");
+            }
+
+            totalVariation += weeklyVariation;
+        }
+
+        const totalCell = cryptoRow.insertCell(data.length + 1);
+        const totalValue = totalVariation.toFixed(2);
+
+        totalCell.style.textAlign = 'center';
+
+        const cryptoNamesElement = document.getElementById('cryptoNames');
+
+        if (limit === 9) {
+            const response21 = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=21`);
+            const data21 = await response21.json();
+            let totalVariation21 = 0;
+            for (let i = 0; i < data21.length; i++) {
+                const openPrice = parseFloat(data21[i][1]);
+                const closePrice = parseFloat(data21[i][4]);
+                totalVariation21 += ((closePrice - openPrice) / openPrice) * 100;
+            }
+            if (totalVariation > totalVariation21) {
+                totalCell.classList.add("positive");
+                cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="positive">${symbol}: LONG, ${totalValue}%</p>`;
+            }
+        }
+
+        totalCell.textContent = `${totalValue}%`;
+
+    } catch (error) {
+        console.error(`Erreur lors de la récupération des données pour ${symbol}:`, error);
+    }
+}
+
+function mettreAJourHeure() {
+    var elementHeure = document.getElementById('heure');
+    var maintenant = new Date();
+
+    var heureActuelle = new Date(maintenant);
+
+    maintenant.setHours(maintenant.getHours() + 3);
+    maintenant.setMinutes(maintenant.getMinutes() + 20);
+
+    var heuresMaintenant = maintenant.getHours();
+    var minutesMaintenant = maintenant.getMinutes();
+    var secondesMaintenant = maintenant.getSeconds();
+
+    var heuresActuelle = heureActuelle.getHours();
+    var minutesActuelle = heureActuelle.getMinutes();
+    var secondesActuelle = heureActuelle.getSeconds();
+
+    heuresMaintenant = heuresMaintenant < 10 ? '0' + heuresMaintenant : heuresMaintenant;
+    minutesMaintenant = minutesMaintenant < 10 ? '0' + minutesMaintenant : minutesMaintenant;
+    secondesMaintenant = secondesMaintenant < 10 ? '0' + secondesMaintenant : secondesMaintenant;
+
+    heuresActuelle = heuresActuelle < 10 ? '0' + heuresActuelle : heuresActuelle;
+    minutesActuelle = minutesActuelle < 10 ? '0' + minutesActuelle : minutesActuelle;
+    secondesActuelle = secondesActuelle < 10 ? '0' + secondesActuelle : secondesActuelle;
+
+    elementHeure.innerHTML = heuresActuelle + ':' + minutesActuelle + ':' + secondesActuelle;
+}
+
+async function fetchAndDisplayAllCryptoData() {
+    mettreAJourHeure();
+
+    const symbols = ["1INCH", "AAVE", "ACE", "ACH", "ADA", "AEVO", "AGIX","AGLD","ALGO","ALICE","ALPHA","ALT","AMB","ANKR","ANT","APE","API3","APT","AR","ARB","ARK","ARKM","ARPA","ASTR","ATA","ATOM","AUCTION","AUDIO","AVAX","AXL","AXS","BADGER","BAKE","BAL","BAND","BAT","BCH","BEAMX","BEL","BICO","BLUR","BLZ","BNB","BNT","BNX","BOME","BOND","BONK","BTC","C98","CAKE","CELO","CELR","CFX","CHR","CHZ","COMBO","COMP","COTI","CRV","CTK","CTSI","CVX","CYBER","DAR","DASH","DENT","DGB","DOGE","DOT","DUSK","DYM","DYDX","EDU","EGLD","ENJ","ENS","EOS","ETC","ETH","ETHFI","FET","FIL","FLM","FLOKI","FLOW","FRONT","FTM","FXS","GALA","GAL","GAS","GLMR","GMT","GMX","GRT","GTC","HBAR","HFT","HIFI","HIGH","HOOK","HOT","ICP","ICX","IDEX","ID","ILV","IMX","INJ","IOST","IOTA","IOTX","JASMY","JOE","JTO","JUP","KAVA","KEY","KLAY","KNC","KSM","LDO","LEVER","LINA","LINK","LIT","LOOM","LPT","LQTY","LRC","LSK","LTC","LUNC","MAGIC","MANTA","MANA","MASK","MATIC","MAV","MBL","MDT","MEME","METIS","MINA","MKR","MOVR","MTL","NEAR","NEO","NFP","NKN","NMR","NTRN","OCEAN","OGN","OMG","ONE","ONDO","ONG","ONT","OP","ORBS","ORDI","OXT","PENDLE","PEOPLE","PEPE","PERP","PHB","PIXEL","POLYX","PORTAL","POWR","PYTH","QNT","QTUM","RAD","RDNT","REEF","REN","RLC","RNDR","RONIN","ROSE","RSR","RUNE","RVN","SAND","1000SATS","SEI","SFP","SHIB","SKL","SLP","SNT","SNX","SOL","SPELL","SSV","STEEM","STG","STMX","STORJ","STPT","STRAX","STRK","STX","SUI","SUPER","SUSHI","SXP","THETA","TIA","TLM","TRB","TRU","TRX","T","TWT","UMA","UNFI","UNI","USDC","USTC","USDT","VET","WAVES","WAXP","WIF","WLD","WOO","XEM","XLM","XRP","XTZ","XVG","XVS","YFI","YGG","ZEC","ZEN","ZIL","ZRX"];
+
+    for (const symbol of symbols) {
+        await fetchAndDisplayCryptoData(symbol, 9);
+    }
+}
+
+fetchAndDisplayAllCryptoData();
