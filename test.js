5ff6933f53e1670c97d10eddf9a3a456d2980ea7
kora1348 <amelianekor@gmail.com>
2024-05-25 13:23:02 +0200

    toto

--- test.js
+++ test.js
@@ -1,106 +1,62 @@
-async function fetchCryptoData(symbol) {
+const symbols = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT'];
+const interval = '1m';
+const ma7Period = 7;
+const ma25Period = 25;
+
+const fetchKlines = async (symbol) => {
     try {
-        const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1m&limit=50`);
+        const response = await fetch(
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${ma25Period}`
+        );
         const data = await response.json();
-
-        let totalVariation = 0;
-        const cryptoRow = document.getElementById(symbol) || document.getElementById('cryptoTable').insertRow();
-        cryptoRow.id = symbol;
-
-        if (!document.getElementById(symbol)) {
-            const cryptoNameCell = cryptoRow.insertCell(0);
-            cryptoNameCell.textContent = symbol;
-        }
-
-        for (let i = 0; i < data.length; i++) {
-            const openPrice = parseFloat(data[i][1]);
-            const closePrice = parseFloat(data[i][4]);
-            const intervalVariation = ((closePrice - openPrice) / openPrice) * 100;
-            const cellIndex = i + 1;
-
-            const variationCell = cryptoRow.insertCell(cellIndex);
-            const variationValue = intervalVariation.toFixed(2);
-            const intervalStartDate = new Date(data[i][0]);
-            const intervalEndDate = new Date(data[i][6]);
-            const optionsStart = { year: "2-digit", month: "2-digit", day: "2-digit", hour: "numeric", minute: "numeric" };
-            const optionsEnd = { hour: "numeric", minute: "numeric" };
-            variationCell.textContent = `${intervalStartDate.toLocaleDateString("fr-FR", optionsStart)} (${intervalStartDate.toLocaleTimeString("fr-FR", optionsEnd)}) - ${intervalEndDate.toLocaleDateString("fr-FR", optionsStart)} (${intervalEndDate.toLocaleTimeString("fr-FR", optionsEnd)}): ${variationValue}%`;
-
-            if (intervalVariation > 0) {
-                variationCell.classList.add("positive");
-            } else if (intervalVariation < 0) {
-                variationCell.classList.add("negative");
-            }
-
-            totalVariation += intervalVariation;
-        }
-
-        const averageVariation = totalVariation / data.length;
-        const averageValue = averageVariation.toFixed(2);
-        const averageCell = cryptoRow.insertCell(data.length + 1);
-        averageCell.style.textAlign = 'center';
-        averageCell.textContent = `Moyenne: ${averageValue}%`;
-
-        if (averageVariation < 0) {
-            averageCell.classList.add("negative");
-        } else {
-            averageCell.classList.add("positive");
-        }
-
-        const cryptoNamesElement = document.getElementById('cryptoNames');
-        if (averageVariation <= -0.03) {
-            cryptoNamesElement.innerHTML += `<p id="${symbol}_status" class="negative">${symbol}: ${averageValue}%</p>`;
-        }
-
+        return data;
     } catch (error) {
         console.error(`Erreur lors de la récupération des données pour ${symbol}:`, error);
     }
-}
+};
 
-function mettreAJourHeure() {
-    const elementHeure = document.getElementById('heure');
-    const maintenant = new Date();
-    const heureActuelle = new Date(maintenant);
+const calculateMA = (data, period) => {
+    let sum = 0;
+    for (let i = 0; i < period; i++) {
+        sum += parseFloat(data[i][4]); // Prix de clôture
+    }
+    return sum / period;
+};
 
-    maintenant.setHours(maintenant.getHours() + 3);
-    maintenant.setMinutes(maintenant.getMinutes() + 20);
+// Créer un objet pour stocker les valeurs précédentes pour chaque symbole
+const previousValues = {};
 
-    const heuresMaintenant = maintenant.getHours().toString().padStart(2, '0');
-    const minutesMaintenant = maintenant.getMinutes().toString().padStart(2, '0');
-    const secondesMaintenant = maintenant.getSeconds().toString().padStart(2, '0');
+const checkCross = async () => {
+    for (const symbol of symbols) {
+        const data = await fetchKlines(symbol);
+        if (!data) return;
 
-    const heuresActuelle = heureActuelle.getHours().toString().padStart(2, '0');
-    const minutesActuelle = heureActuelle.getMinutes().toString().padStart(2, '0');
-    const secondesActuelle = heureActuelle.getSeconds().toString().padStart(2, '0');
+        const ma7 = calculateMA(data.slice(0, ma7Period), ma7Period);
+        const ma25 = calculateMA(data.slice(0, ma25Period), ma25Period);
 
-    elementHeure.innerHTML = `${heuresActuelle}:${minutesActuelle}:${secondesActuelle} - ${heuresMaintenant}:${minutesMaintenant}:${secondesMaintenant}`;
-}
+        // Initialiser les valeurs précédentes si elles n'existent pas
+        if (!previousValues[symbol]) {
+            previousValues[symbol] = { previousMA7: ma7, previousMA25: ma25 };
+            continue; // Continuer au prochain symbole sans vérifier les croisements pour cette itération
+        }
 
-const cryptoSymbols = [
-    "1INCH", "AAVE", "ACE", "ACH", "ADA", "AEVO", "AGIX", "AGLD", "ALGO", "ALICE", "ALPHA", "ALT", "AMB", "ANKR", "ANT", "APE", "API3", "APT", "AR", "ARB",
-    "ARK", "ARKM", "ARPA", "ASTR", "ATA", "ATOM", "AUCTION", "AUDIO", "AVAX", "AXL", "AXS", "BADGER", "BAKE", "BAL", "BAND", "BAT", "BCH", "BEAMX", "BEL",
-    "BICO", "BLUR", "BLZ", "BNB", "BNT", "BNX", "BOME", "BOND", "BONK", "BTC", "C98", "CAKE", "CELO", "CELR", "CFX", "CHR", "CHZ", "COMBO", "COMP", "COTI",
-    "CRV", "CTK", "CTSI", "CVX", "CYBER", "DAR", "DASH", "DENT", "DGB", "DOGE", "DOT", "DUSK", "DYM", "DYDX", "EDU", "EGLD", "ENJ", "ENS", "EOS", "ETC",
-    "ETH", "ETHFI", "FET", "FIL", "FLM", "FLOKI", "FLOW", "FRONT", "FTM", "FXS", "GALA", "GAL", "GAS", "GLMR", "GMT", "GMX", "GRT", "GTC", "HBAR", "HFT",
-    "HIFI", "HIGH", "HOOK", "HOT", "ICP", "ICX", "IDEX", "ID", "ILV", "IMX", "INJ", "IOST", "IOTA", "IOTX", "JASMY", "JOE", "JTO", "JUP", "KAVA", "KEY",
-    "KLAY", "KNC", "KSM", "LDO", "LEVER", "LINA", "LINK", "LIT", "LOOM", "LPT", "LQTY", "LRC", "LSK", "LTC", "LUNC", "MAGIC", "MANTA", "MANA", "MASK",
-    "MATIC", "MAV", "MBL", "MDT", "MEME", "METIS", "MINA", "MKR", "MOVR", "MTL", "NEAR", "NEO", "NFP", "NKN", "NMR", "NTRN", "OCEAN", "OGN", "OMG", "ONE",
-    "ONDO", "ONG", "ONT", "OP", "ORBS", "ORDI", "OXT", "PENDLE", "PEOPLE", "PEPE", "PERP", "PHB", "PIXEL", "POLYX", "PORTAL", "POWR", "PYTH", "QNT", "QTUM",
-    "RAD", "RDNT", "REEF", "REN", "RLC", "RNDR", "RONIN", "ROSE", "RSR", "RUNE", "RVN", "SAND", "1000SATS", "SEI", "SFP", "SHIB", "SKL", "SLP", "SNT",
-    "SNX", "SOL", "SPELL", "SSV", "STEEM", "STG", "STMX", "STORJ", "STPT", "STRAX", "STRK", "STX", "SUI", "SUPER", "SUSHI", "SXP", "THETA", "TIA", "TLM",
-    "TRB", "TRU", "TRX", "T", "TWT", "UMA", "UNFI", "UNI", "USDC", "USTC", "USDT", "VET", "WAVES", "WAXP", "WIF", "WLD", "WOO", "XEM", "XLM", "XRP", "XTZ",
-    "XVG", "XVS", "YFI", "YGG", "ZEC", "ZEN", "ZIL", "ZRX"
-];
+        const { previousMA7, previousMA25 } = previousValues[symbol];
 
-function updateAllCryptoData() {
-    document.getElementById('cryptoTable').querySelector('tbody').innerHTML = ''; // Clear existing rows
-    document.getElementById('cryptoNames').innerHTML = ''; // Clear existing status
-    cryptoSymbols.forEach(symbol => fetchCryptoData(symbol));
-}
+        // Condition pour MA7 croissant MA25
+        if (ma7 > ma25 && previousMA7 <= previousMA25) {
+            document.getElementById('alert').innerText = `MA7 has crossed above MA25 for ${symbol}!`;
+            document.getElementById('alert').classList.add('positive');
+        }
 
-// Mettre à jour l'heure toutes les secondes
-setInterval(mettreAJourHeure, 1000);
+        // Condition pour MA25 croissant MA7
+        if (ma25 > ma7 && previousMA25 <= previousMA7) {
+            document.getElementById('alert').innerText = `MA25 has crossed above MA7 for ${symbol}!`;
+            document.getElementById('alert').classList.add('negative');
+        }
 
-// Mettre à jour les données toutes les minutes
-updateAllCryptoData();
-setInterval(updateAllCryptoData, 60000);
+        // Mettre à jour les valeurs précédentes
+        previousValues[symbol] = { previousMA7: ma7, previousMA25: ma25 };
+    }
+};
+
+setInterval(checkCross, 60000); // Vérifier chaque minute
