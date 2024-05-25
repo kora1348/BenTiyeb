3c5cd34cb23c3bb646997b19aae43716adaee69b
kora1348 <amelianekor@gmail.com>
2024-05-25 15:16:55 +0200

    tt

--- test.js
+++ test.js
@@ -69,5 +69,39 @@ const checkCross = async () => {
     }
 };
 
-setInterval(checkCross, 60000); // Vérifier chaque minute
 checkCross(); // Appeler une fois immédiatement pour avoir des données dès le chargement
+
+function mettreAJourHeure() {
+    var elementHeure = document.getElementById('heure');
+    var maintenant = new Date();
+
+    // Créer une copie de l'heure actuelle
+    var heureActuelle = new Date(maintenant);
+
+    // Ajouter 3 heures et 20 minutes à l'heure actuelle
+    maintenant.setHours(maintenant.getHours() + 3);
+    maintenant.setMinutes(maintenant.getMinutes() + 20);
+
+    var heuresMaintenant = maintenant.getHours();
+    var minutesMaintenant = maintenant.getMinutes();
+    var secondesMaintenant = maintenant.getSeconds();
+
+    var heuresActuelle = heureActuelle.getHours();
+    var minutesActuelle = heureActuelle.getMinutes();
+    var secondesActuelle = heureActuelle.getSeconds();
+
+    // Ajouter un zéro devant les chiffres < 10
+    heuresMaintenant = heuresMaintenant < 10 ? '0' + heuresMaintenant : heuresMaintenant;
+    minutesMaintenant = minutesMaintenant < 10 ? '0' + minutesMaintenant : minutesMaintenant;
+    secondesMaintenant = secondesMaintenant < 10 ? '0' + secondesMaintenant : secondesMaintenant;
+
+    heuresActuelle = heuresActuelle < 10 ? '0' + heuresActuelle : heuresActuelle;
+    minutesActuelle = minutesActuelle < 10 ? '0' + minutesActuelle : minutesActuelle;
+    secondesActuelle = secondesActuelle < 10 ? '0' + secondesActuelle : secondesActuelle;
+
+    // Mettre à jour le contenu de l'élément avec les deux heures
+    elementHeure.innerHTML = heuresActuelle + ':' + minutesActuelle + ':' + secondesActuelle;
+}
+
+// Appeler la fonction pour mettre à jour l'heure
+mettreAJourHeure();
