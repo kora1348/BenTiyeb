f9f91bcb9ca1be829e1700cb531745d41bf1448f
kora1348 <amelianekor@gmail.com>
2024-06-05 20:06:42 +0200

    toto

--- 1.js
+++ 1.js
@@ -73,11 +73,13 @@ async function fetchCryptoData(symbol) {
         const interval2 = data.findIndex(d => new Date(d[0]).getTime() === top2.date.getTime());
         const nombreIntervalles = Math.abs(interval1 - interval2) - 1;
 
-        // Ajouter les informations spécifiques pour chaque crypto
-        const cryptoList = document.getElementById('cryptoList');
-        const cryptoInfo = `${symbol} : ${top1.variation.toFixed(2)}% (${top1.date.toLocaleDateString("fr-FR")}) - ${top2.variation.toFixed(2)}% (${top2.date.toLocaleDateString("fr-FR")}) = ${nombreIntervalles}`;
+        // Ajout des informations spécifiques pour ETH
+        if (symbol === 'ETH') {
+            const cryptoList = document.getElementById('cryptoList');
+            const cryptoInfo = `ETH : ${top1.variation.toFixed(2)}% (${top1.date.toLocaleDateString("fr-FR")}) - ${top2.variation.toFixed(2)}% (${top2.date.toLocaleDateString("fr-FR")}) = ${nombreIntervalles}`;
 
-        cryptoList.innerHTML += `<p>${cryptoInfo}</p>`;
+            cryptoList.textContent = cryptoInfo;
+        }
 
     } catch (error) {
         console.error(
@@ -87,40 +89,6 @@ async function fetchCryptoData(symbol) {
     }
 }
 
-// Liste des cryptos
-const cryptos = [
-    '1INCH', 'AAVE', 'ACE', 'ACH', 'ADA', 'AEVO', 'AGIX', 'AGLD', 'ALGO',
-    'ALICE', 'ALPHA', 'ALT', 'AMB', 'ANKR', 'ANT', 'APE', 'API3', 'APT',
-    'AR', 'ARB', 'ARK', 'ARKM', 'ARPA', 'ASTR', 'ATA', 'ATOM', 'AUCTION',
-    'AUDIO', 'AVAX', 'AXL', 'AXS', 'BADGER', 'BAKE', 'BAL', 'BAND', 'BAT',
-    'BCH', 'BEAMX', 'BEL', 'BICO', 'BLUR', 'BLZ', 'BNB', 'BNT', 'BNX',
-    'BOME', 'BOND', 'BONK', 'BTC', 'C98', 'CAKE', 'CELO', 'CELR', 'CFX',
-    'CHR', 'CHZ', 'COMBO', 'COMP', 'COTI', 'CRV', 'CTK', 'CTSI', 'CVX',
-    'CYBER', 'DAR', 'DASH', 'DENT', 'DGB', 'DOGE', 'DOT', 'DUSK', 'DYM',
-    'DYDX', 'EDU', 'EGLD', 'ENJ', 'ENS', 'EOS', 'ETC', 'ETH', 'ETHFI',
-    'FET', 'FIL', 'FLM', 'FLOKI', 'FLOW', 'FRONT', 'FTM', 'FXS', 'GALA',
-    'GAL', 'GAS', 'GLMR', 'GMT', 'GMX', 'GRT', 'GTC', 'HBAR', 'HFT',
-    'HIFI', 'HIGH', 'HOOK', 'HOT', 'ICP', 'ICX', 'IDEX', 'ID', 'ILV',
-    'IMX', 'INJ', 'IOST', 'IOTA', 'IOTX', 'JASMY', 'JOE', 'JTO', 'JUP',
-    'KAVA', 'KEY', 'KLAY', 'KNC', 'KSM', 'LDO', 'LEVER', 'LINA', 'LINK',
-    'LIT', 'LOOM', 'LPT', 'LQTY', 'LRC', 'LSK', 'LTC', 'LUNC', 'MAGIC',
-    'MANTA', 'MANA', 'MASK', 'MATIC', 'MAV', 'MBL', 'MDT', 'MEME', 'METIS',
-    'MINA', 'MKR', 'MOVR', 'MTL', 'NEAR', 'NEO', 'NFP', 'NKN', 'NMR',
-    'NTRN', 'OCEAN', 'OGN', 'OMG', 'ONE', 'ONDO', 'ONG', 'ONT', 'OP',
-    'ORBS', 'ORDI', 'OXT', 'PENDLE', 'PEOPLE', 'PEPE', 'PERP', 'PHB',
-    'PIXEL', 'POLYX', 'PORTAL', 'POWR', 'PYTH', 'QNT', 'QTUM', 'RAD',
-    'RDNT', 'REEF', 'REN', 'RLC', 'RNDR', 'RONIN', 'ROSE', 'RSR', 'RUNE',
-    'RVN', 'SAND', '1000SATS', 'SEI', 'SFP', 'SHIB', 'SKL', 'SLP', 'SNT',
-    'SNX', 'SOL', 'SPELL', 'SSV', 'STEEM', 'STG', 'STMX', 'STORJ', 'STPT',
-    'STRAX', 'STRK', 'STX', 'SUI', 'SUPER', 'SUSHI', 'SXP', 'THETA',
-    'TIA', 'TLM', 'TRB', 'TRU', 'TRX', 'T', 'TWT', 'UMA', 'UNFI', 'UNI',
-    'USDC', 'USTC', 'USDT', 'VET', 'WAVES', 'WAXP', 'WIF', 'WLD', 'WOO',
-    'XEM', 'XLM', 'XRP', 'XTZ', 'XVG', 'XVS', 'YFI', 'YGG', 'ZEC', 'ZEN',
-    'ZIL', 'ZRX'
-];
-
-// Appel de la fonction pour chaque crypto
-cryptos.forEach(crypto => fetchCryptoData(crypto));
 
 function mettreAJourHeure() {
 	var elementHeure = document.getElementById('heure');
