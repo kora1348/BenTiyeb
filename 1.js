ded44765a36ea23d6463fa2b3cb994b3777336f2
kora1348 <amelianekor@gmail.com>
2024-06-05 20:16:17 +0200

    toto

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1M`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1m`
         );
         const data = await response.json();
 
@@ -15,28 +15,33 @@ async function fetchCryptoData(symbol) {
         let totalVariation = 0;
         let variations = [];
 
-        for (let i = 0; i < data.length; i++) {
+        // Obtenir l'heure actuelle
+        const currentDate = new Date();
+        const currentHour = currentDate.getHours();
+
+        // Récupérer les données de variation pour les 24 dernières heures
+        for (let i = data.length - currentHour - 1; i < data.length; i++) {
             const openPrice = parseFloat(data[i][1]);
             const closePrice = parseFloat(data[i][4]);
-            const monthlyVariation = ((closePrice - openPrice) / openPrice) * 100;
+            const hourlyVariation = ((closePrice - openPrice) / openPrice) * 100;
             const variationCell = document.createElement('td');
-            const variationValue = monthlyVariation.toFixed(2);
-            const monthStartDate = new Date(data[i][0]);
+            const variationValue = hourlyVariation.toFixed(2);
+            const hourStartDate = new Date(data[i][0]);
 
-            variationCell.textContent = `${monthStartDate.toLocaleDateString("fr-FR")}: ${variationValue}%`;
+            variationCell.textContent = `${hourStartDate.toLocaleTimeString("fr-FR")}: ${variationValue}%`;
 
-            // Ajouter la classe "positive" ou "negative" en fonction de la variation mensuelle
-            if (monthlyVariation > 0) {
+            // Ajouter la classe "positive" ou "negative" en fonction de la variation horaire
+            if (hourlyVariation > 0) {
                 variationCell.classList.add("positive");
-            } else if (monthlyVariation < 0) {
+            } else if (hourlyVariation < 0) {
                 variationCell.classList.add("negative");
             }
 
             cryptoRow.appendChild(variationCell);
-            totalVariation += monthlyVariation; // Ajouter la variation mensuelle au total
+            totalVariation += hourlyVariation; // Ajouter la variation horaire au total
 
             // Stocker les variations et les dates pour l'analyse ultérieure
-            variations.push({ date: monthStartDate, variation: monthlyVariation });
+            variations.push({ date: hourStartDate, variation: hourlyVariation });
         }
 
         // Ajouter la cellule pour afficher le total de variation
@@ -73,13 +78,11 @@ async function fetchCryptoData(symbol) {
         const interval2 = data.findIndex(d => new Date(d[0]).getTime() === top2.date.getTime());
         const nombreIntervalles = Math.abs(interval1 - interval2) - 1;
 
-        // Ajout des informations spécifiques pour ETH
-        if (symbol === 'ETH') {
-            const cryptoList = document.getElementById('cryptoList');
-            const cryptoInfo = `ETH : ${top1.variation.toFixed(2)}% (${top1.date.toLocaleDateString("fr-FR")}) - ${top2.variation.toFixed(2)}% (${top2.date.toLocaleDateString("fr-FR")}) = ${nombreIntervalles}`;
+        // Ajouter les informations spécifiques pour chaque crypto
+        const cryptoList = document.getElementById('cryptoList');
+        const cryptoInfo = `${symbol} : ${top1.variation.toFixed(2)}% (${top1.date.toLocaleTimeString("fr-FR")}) - ${top2.variation.toFixed(2)}% (${top2.date.toLocaleTimeString("fr-FR")}) = ${nombreIntervalles}`;
 
-            cryptoList.textContent = cryptoInfo;
-        }
+        cryptoList.innerHTML += `<p>${cryptoInfo}</p>`;
 
     } catch (error) {
         console.error(
@@ -89,6 +92,40 @@ async function fetchCryptoData(symbol) {
     }
 }
 
+
+// Liste des cryptos
+const cryptos = [
+    '1INCH', 'AAVE', 'ACE', 'ACH', 'ADA', 'AEVO', 'AGIX', 'AGLD', 'ALGO',
+    'ALICE', 'ALPHA', 'ALT', 'AMB', 'ANKR', 'ANT', 'APE', 'API3', 'APT',
+    'AR', 'ARB', 'ARK', 'ARKM', 'ARPA', 'ASTR', 'ATA', 'ATOM', 'AUCTION',
+    'AUDIO', 'AVAX', 'AXL', 'AXS', 'BADGER', 'BAKE', 'BAL', 'BAND', 'BAT',
+    'BCH', 'BEAMX', 'BEL', 'BICO', 'BLUR', 'BLZ', 'BNB', 'BNT', 'BNX',
+    'BOME', 'BOND', 'BONK', 'BTC', 'C98', 'CAKE', 'CELO', 'CELR', 'CFX',
+    'CHR', 'CHZ', 'COMBO', 'COMP', 'COTI', 'CRV', 'CTK', 'CTSI', 'CVX',
+    'CYBER', 'DAR', 'DASH', 'DENT', 'DGB', 'DOGE', 'DOT', 'DUSK', 'DYM',
+    'DYDX', 'EDU', 'EGLD', 'ENJ', 'ENS', 'EOS', 'ETC', 'ETH', 'ETHFI',
+    'FET', 'FIL', 'FLM', 'FLOKI', 'FLOW', 'FRONT', 'FTM', 'FXS', 'GALA',
+    'GAL', 'GAS', 'GLMR', 'GMT', 'GMX', 'GRT', 'GTC', 'HBAR', 'HFT',
+    'HIFI', 'HIGH', 'HOOK', 'HOT', 'ICP', 'ICX', 'IDEX', 'ID', 'ILV',
+    'IMX', 'INJ', 'IOST', 'IOTA', 'IOTX', 'JASMY', 'JOE', 'JTO', 'JUP',
+    'KAVA', 'KEY', 'KLAY', 'KNC', 'KSM', 'LDO', 'LEVER', 'LINA', 'LINK',
+    'LIT', 'LOOM', 'LPT', 'LQTY', 'LRC', 'LSK', 'LTC', 'LUNC', 'MAGIC',
+    'MANTA', 'MANA', 'MASK', 'MATIC', 'MAV', 'MBL', 'MDT', 'MEME', 'METIS',
+    'MINA', 'MKR', 'MOVR', 'MTL', 'NEAR', 'NEO', 'NFP', 'NKN', 'NMR',
+    'NTRN', 'OCEAN', 'OGN', 'OMG', 'ONE', 'ONDO', 'ONG', 'ONT', 'OP',
+    'ORBS', 'ORDI', 'OXT', 'PENDLE', 'PEOPLE', 'PEPE', 'PERP', 'PHB',
+    'PIXEL', 'POLYX', 'PORTAL', 'POWR', 'PYTH', 'QNT', 'QTUM', 'RAD',
+    'RDNT', 'REEF', 'REN', 'RLC', 'RNDR', 'RONIN', 'ROSE', 'RSR', 'RUNE',
+    'RVN', 'SAND', '1000SATS', 'SEI', 'SFP', 'SHIB', 'SKL', 'SLP', 'SNT',
+    'SNX', 'SOL', 'SPELL', 'SSV', 'STEEM', 'STG', 'STMX', 'STORJ', 'STPT',
+    'STRAX', 'STRK', 'STX', 'SUI', 'SUPER', 'SUSHI', 'SXP', 'THETA',
+    'TIA', 'TLM', 'TRB', 'TRU', 'TRX', 'T', 'TWT', 'UMA', 'UNFI', 'UNI',
+    'USDC', 'USTC', 'USDT', 'VET', 'WAVES', 'WAXP', 'WIF', 'WLD', 'WOO',
+    'XEM', 'XLM', 'XRP', 'XTZ', 'XVG', 'XVS', 'YFI', 'YGG', 'ZEC', 'ZEN',
+    'ZIL', 'ZRX'
+];
+
+cryptos.forEach(crypto => fetchCryptoData(crypto));
 
 function mettreAJourHeure() {
 	var elementHeure = document.getElementById('heure');
