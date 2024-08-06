6bf663a05a664849385872a2d8e63b5123ad5332
kora1348 <amelianekor@gmail.com>
2024-08-06 17:19:28 +0200

    toto

--- 1h.js
+++ 1h.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=15m&limit=6`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=6`
         );
         const data = await response.json();
 
--- 30m.js
+++ 30m.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=15m&limit=6`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=30m&limit=6`
         );
         const data = await response.json();
 
