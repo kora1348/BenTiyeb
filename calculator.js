db6fd741ad4684236118aedfe12570467854bf40
kora1348 <amelianekor@gmail.com>
2024-08-28 11:39:32 +0200

    totot

--- calculator.js
+++ calculator.js
@@ -5,16 +5,16 @@ function calculate() {
     // Calcul des résultats pour les multiplications positives (affichage inverse)
     for (let i = 10; i >= 1; i--) {
         const result = number + (number * i / 100);
-        output += `<div class="positive">${result.toFixed(2)}</div>`;
+        output += `<div class="positive">${result.toFixed(4)}</div>`;
     }
 
     // Ajout de la valeur de l'input avec la classe "blue"
-    output += `<div class="blue">${number.toFixed(2)}</div>`;
+    output += `<div class="blue">${number.toFixed(4)}*</div>`;
 
     // Calcul des résultats pour les multiplications négatives
     for (let i = -1; i >= -10; i--) {
         const result = number + (number * i / 100);
-        output += `<div class="negative">${result.toFixed(2)}</div>`;
+        output += `<div class="negative">${result.toFixed(4)}</div>`;
     }
 
     // Affichage des résultats
