0620870f0ff430f93a90e05a8d8987e2f30eea51
kora1348 <amelianekor@gmail.com>
2024-09-21 12:26:23 +0200

    toto

--- 5m.js
+++ 5m.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=24`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=60`
         );
         const data = await response.json();
 
