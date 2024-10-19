7bf17cf69dc8719a71937592a25a593c3cdccad6
kora1348 <amelianekor@gmail.com>
2024-10-20 00:02:51 +0200

    ttoe

--- analyse/calculator.js
+++ analyse/calculator.js
@@ -5,7 +5,7 @@ function calculate() {
     // Calcul des résultats pour les multiplications positives (affichage inverse)
     for (let i = 10; i >= 1; i--) {
         const result = number + (number * i / 100);
-        output += `<div class="positive">${result.toFixed(4)}</div>`;
+        output += `<div class="negative">${result.toFixed(4)}</div>`;
     }
 
     // Ajout de la valeur de l'input avec la classe "blue"
@@ -14,7 +14,7 @@ function calculate() {
     // Calcul des résultats pour les multiplications négatives
     for (let i = -1; i >= -10; i--) {
         const result = number + (number * i / 100);
-        output += `<div class="negative">${result.toFixed(4)}</div>`;
+        output += `<div class="positive">${result.toFixed(4)}</div>`;
     }
 
     // Affichage des résultats
