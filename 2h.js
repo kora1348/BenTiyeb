8b1a14c26fb129e48c7bf258bfee2ccf571185a3
kora1348 <amelianekor@gmail.com>
2024-11-02 13:25:48 +0100

    02-11-24

--- 2h.js
+++ 2h.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=60`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=2h&limit=60`
         );
         const data = await response.json();
 
