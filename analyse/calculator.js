6d5f5071cf180e7d8ca47e7cc53d35e62e09e324
kora1348 <amelianekor@gmail.com>
2024-11-03 14:51:07 +0100

    toto

--- analyse/calculator.js
+++ analyse/calculator.js
@@ -28,7 +28,7 @@ function calculateScalping() {
     // Calcul des résultats pour les multiplications positives (affichage inversé)
     for (let i = 10; i >= 1; i--) { // Boucle dans l'ordre décroissant
         const result = number + (number * (i * 0.1) / 100); // Utilisation de 0,1 % par itération
-        output += `<div class="negative">${result.toFixed(4)}</div>`;
+        output += `<div class="negative">${result.toFixed(5)}</div>`;
     }
 
     // Ajout de la valeur de l'input avec la classe "blue"
@@ -37,7 +37,7 @@ function calculateScalping() {
     // Calcul des résultats pour les multiplications négatives (affichage inversé)
     for (let i = 1; i <= 10; i++) {
         const result = number - (number * (i * 0.1) / 100); // Utilisation de 0,1 % par itération
-        output += `<div class="positive">${result.toFixed(4)}</div>`;
+        output += `<div class="positive">${result.toFixed(5)}</div>`;
     }
 
     // Affichage des résultats
