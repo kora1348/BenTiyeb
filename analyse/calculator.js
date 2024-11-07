20880f07be1392ef338f8e0d6dc9e0c72000d990
kora1348 <amelianekor@gmail.com>
2024-11-07 21:51:13 +0100

    toto

--- analyse/calculator.js
+++ analyse/calculator.js
@@ -25,21 +25,18 @@ function calculateDay() {
     const number = parseFloat(document.getElementById('numberInputS').value);
     let output = '';
 
-    // Calcul des résultats pour les multiplications positives (affichage inversé)
-    for (let i = 10; i >= 1; i--) {
-        const result = number + (number * i / 100);
-        output += `<div class="negative">${result.toFixed(10)}</div>`;
-    }
+    // Calcul de +5%
+    const resultPlus5 = number + (number * 5 / 100);
+    output += `<div class="negative">${resultPlus5.toFixed(10)}</div>`;
 
     // Ajout de la valeur de l'input avec la classe "blue"
     output += `<div class="blue">${number.toFixed(10)}*</div>`;
 
-    // Calcul des résultats pour les multiplications négatives (affichage inversé)
-    for (let i = -1; i >= -10; i--) {
-        const result = number + (number * i / 100);
-        output += `<div class="positive">${result.toFixed(10)}</div>`;
-    }
+    // Calcul de -5%
+    const resultMinus5 = number - (number * 5 / 100);
+    output += `<div class="positive">${resultMinus5.toFixed(10)}</div>`;
 
     // Affichage des résultats
     document.getElementById('outputS').innerHTML = output;
 }
+
