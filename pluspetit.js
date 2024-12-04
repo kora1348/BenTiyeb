a8473c79b85e0d1350996774da63a764ea957b07
kora1348 <amelianekor@gmail.com>
2024-12-04 23:25:12 +0100

    toto

--- pluspetit.js
+++ pluspetit.js
@@ -24,7 +24,7 @@ function showPopup(message) {
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=5`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=178`
         );
         const data = await response.json();
 
