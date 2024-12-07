865c78d69fc7d33ed09a50dba1d41f4b7b6cdf70
kora1348 <amelianekor@gmail.com>
2024-12-07 15:52:43 +0100

    toto

--- long/5m.js
+++ long/5m.js
@@ -24,7 +24,7 @@ function showPopup(message) {
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=178`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=5m&limit=185`
         );
         const data = await response.json();
 
