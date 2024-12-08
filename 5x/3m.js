a76846d52a578f2a6cdb477b8fe353aaa1fc47df
kora1348 <amelianekor@gmail.com>
2024-12-08 23:09:28 +0100

    tot

--- 5x/3m.js
+++ 5x/3m.js
@@ -29,7 +29,7 @@ function clearNotifications() {
 async function fetchCryptoData(symbol) {
     try {
         const response = await fetch(
-            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=3m&limit=1`
+            `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=3m&limit=6`
         );
         const data = await response.json();
 
@@ -38,13 +38,14 @@ async function fetchCryptoData(symbol) {
             return;
         }
 
-        let totalVariation = 0;
         const cryptoRow = document.getElementById(symbol);
 
         // Réinitialiser les cellules
         while (cryptoRow.cells.length > 1) {
             cryptoRow.deleteCell(1);
         }
+
+        let lastIntervalVariation = 0;
 
         for (let i = 0; i < data.length; i++) {
             const openPrice = parseFloat(data[i][1]);
@@ -71,38 +72,43 @@ async function fetchCryptoData(symbol) {
 
             variationCell.classList.add(variation > 0 ? "positive" : "negative");
 
-            totalVariation += variation;
+            // Stocker la variation du dernier intervalle (6ème)
+            if (i === data.length - 1) {
+                lastIntervalVariation = variation;
+            }
         }
 
         const totalCell = cryptoRow.insertCell(-1);
-        totalCell.textContent = `${totalVariation.toFixed(2)}%`;
+        totalCell.textContent = `${lastIntervalVariation.toFixed(2)}%`;
         totalCell.style.textAlign = "center";
 
         // Ne pas effacer cryptoNames ici car on veut garder les éléments affichés
         const cryptoNamesElement = document.getElementById("cryptoNames");
         document.querySelector(`#${symbol}_status`)?.remove();
 
-        if (totalVariation > 5.00 && totalVariation <= 5.99) {
+        if (lastIntervalVariation >= 5.00 && lastIntervalVariation <= 5.99) {
             totalCell.classList.add("positive");
             const pElement = document.createElement("p");
             pElement.id = `${symbol}_status`;
             pElement.classList.add("positive");
-            pElement.textContent = `${symbol}: LONG, ${totalVariation.toFixed(2)}%`;
+            pElement.textContent = `${symbol}: LONG, ${lastIntervalVariation.toFixed(2)}%`;
             cryptoNamesElement.appendChild(pElement);
-            showPopup(`${symbol}: LONG signal détecté - 3 MINUTES(${totalVariation.toFixed(2)}%)`);
-        }  else if (totalVariation >= -5.99 && totalVariation <= -5.00) {
+            showPopup(`${symbol}: LONG signal détecté - 3 MINUTES (${lastIntervalVariation.toFixed(2)}%)`);
+        } else if (lastIntervalVariation >= -5.99 && lastIntervalVariation <= -5.30) {
             totalCell.classList.add("negative");
             const pElement = document.createElement("p");
             pElement.id = `${symbol}_status`;
             pElement.classList.add("negative");
-            pElement.textContent = `${symbol}: SHORT, ${totalVariation.toFixed(2)}%`;
+            pElement.textContent = `${symbol}: SHORT, ${lastIntervalVariation.toFixed(2)}%`;
             cryptoNamesElement.appendChild(pElement);
-            showPopup(`${symbol}: SHORT signal détecté - 3 MINUTES(${totalVariation.toFixed(2)}%)`);
+            showPopup(`${symbol}: SHORT signal détecté - 3 MINUTES (${lastIntervalVariation.toFixed(2)}%)`);
         }
     } catch (error) {
         console.error(`Erreur lors de la récupération des données pour ${symbol}:`, error);
     }
 }
+
+
 
 // Fonction pour calculer et ajuster l'intervalle de rafraîchissement
 function calculerProchainRafraichissement() {
@@ -119,25 +125,25 @@ function calculerProchainRafraichissement() {
 // Démarrage de l'actualisation
 function startAutoRefresh() {
     const cryptoSymbols = ["1INCH", "AAVE", "ACE", "ACH", "ADA", "AEVO", "AGLD", "AI", "ALGO", "ALICE", "ALPACA", "ALPHA",
-    "ALT", "AMB", "ANKR", "APE", "API3", "APT", "ARB", "ARKM", "ARK", "ARPA", "AR", "ASTR", "ATA",
-    "ATOM", "AUCTION", "AVAX", "AXL", "AXS", "BADGER", "BAKE", "BAL", "BANANA", "BAND", "BAT", "BB",
-    "BCH", "BEAMX", "BEL", "BICO", "BLUR", "BLZ", "BNB", "BNT", "BNX", "BOME", "BONK", "BOND", "BTC",
-    "C98", "CAKE", "CELO", "CELR", "CFX", "CHR", "CHZ", "CKB", "COMBO", "COMP", "COTI", "CRV",
-    "CTSI", "CYBER", "DAR", "DASH", "DENT", "DOGE", "DOGS", "DOT", "DUSK", "DYDX", "DYM", "EDU",
-    "EGLD", "ENA", "ENJ", "ENS", "EOS", "ETC", "ETHFI", "ETH", "FET", "FIL", "FLM", "FLOKI", "FLOW",
-    "FTM", "FXS", "GALA", "GAS", "GLM", "GMT", "GMX", "GRT", "GTC", "G", "HBAR", "HFT", "HIFI",
-    "HIGH", "HOOK", "HOT", "ICP", "ICX", "ID", "ILV", "IMX", "INJ", "IOST", "IOTA", "IOTX", "IO",
-    "JASMY", "JOE", "JTO", "JUP", "KAVA", "KEY", "KLAY", "KNC", "KSM", "LDO", "LEVER", "LINA", "LINK", 
-    "LISTA", "LIT", "LOOM", "LPT", "LQTY", "LRC", "LSK", "LTC", "LUNC", "MAGIC", "MANA", "MANTA", 
-    "MASK", "MAV", "MEME", "METIS", "MINA", "MKR", "MOVR", "MTL", "NEAR", "NEO", "NFP", "NKN", 
-    "NMR", "NOT", "NTRN", "OGN", "OMG", "OMNI", "OM", "ONE", "ONG", "ONT", "OP", "ORDI", "OXT", 
-    "PENDLE", "PEOPLE", "PEPE", "PERP", "PHB", "PIXEL", "POLYX", "PORTAL", "POWR", "PYTH", "QNT", 
-    "QTUM", "RARE", "RDNT", "REEF", "RENDER", "REN", "REZ", "RIF", "RLC", "RONIN", "ROSE", "RSR", 
-    "RUNE", "RVN", "SAGA", "SAND", "SEI", "SFP", "SHIB", "SKL", "SNX", "SOL", "SPELL", "SSV", 
-    "STEEM", "STG", "STMX", "STORJ", "STRK", "STX", "SUI", "SUN", "SUPER", "SUSHI", "SXP", "SYN", 
-    "SYS", "TAO", "THETA", "TIA", "TLM", "TNSR", "TON", "TRB", "TRU", "TRX", "TWT", "UMA", "UNFI", 
-    "UNI", "USTC", "VANRY", "VET", "VIDT", "VOXEL", "WAXP", "WIF", "WLD", "WOO", "W", "XAI", "XEC", 
-    "XEM", "XLM", "XRP", "XTZ", "XVG", "XVS", "YFI", "YGG", "ZEC", "ZIL", "ZK", "ZRO", "ZRX"]; // Ajoutez d'autres symboles crypto si nécessaire
+        "ALT", "AMB", "ANKR", "APE", "API3", "APT", "ARB", "ARKM", "ARK", "ARPA", "AR", "ASTR", "ATA",
+        "ATOM", "AUCTION", "AVAX", "AXL", "AXS", "BADGER", "BAKE", "BAL", "BANANA", "BAND", "BAT", "BB",
+        "BCH", "BEAMX", "BEL", "BICO", "BLUR", "BLZ", "BNB", "BNT", "BNX", "BOME", "BONK", "BOND", "BTC",
+        "C98", "CAKE", "CELO", "CELR", "CFX", "CHR", "CHZ", "CKB", "COMBO", "COMP", "COTI", "CRV",
+        "CTSI", "CYBER", "DAR", "DASH", "DENT", "DOGE", "DOGS", "DOT", "DUSK", "DYDX", "DYM", "EDU",
+        "EGLD", "ENA", "ENJ", "ENS", "EOS", "ETC", "ETHFI", "ETH", "FET", "FIL", "FLM", "FLOKI", "FLOW",
+        "FTM", "FXS", "GALA", "GAS", "GLM", "GMT", "GMX", "GRT", "GTC", "G", "HBAR", "HFT", "HIFI",
+        "HIGH", "HOOK", "HOT", "ICP", "ICX", "ID", "ILV", "IMX", "INJ", "IOST", "IOTA", "IOTX", "IO",
+        "JASMY", "JOE", "JTO", "JUP", "KAVA", "KEY", "KLAY", "KNC", "KSM", "LDO", "LEVER", "LINA", "LINK", 
+        "LISTA", "LIT", "LOOM", "LPT", "LQTY", "LRC", "LSK", "LTC", "LUNC", "MAGIC", "MANA", "MANTA", 
+        "MASK", "MAV", "MEME", "METIS", "MINA", "MKR", "MOVR", "MTL", "NEAR", "NEO", "NFP", "NKN", 
+        "NMR", "NOT", "NTRN", "OGN", "OMG", "OMNI", "OM", "ONE", "ONG", "ONT", "OP", "ORDI", "OXT", 
+        "PENDLE", "PEOPLE", "PEPE", "PERP", "PHB", "PIXEL", "POLYX", "PORTAL", "POWR", "PYTH", "QNT", 
+        "QTUM", "RARE", "RDNT", "REEF", "RENDER", "REN", "REZ", "RIF", "RLC", "RONIN", "ROSE", "RSR", 
+        "RUNE", "RVN", "SAGA", "SAND", "SEI", "SFP", "SHIB", "SKL", "SNX", "SOL", "SPELL", "SSV", 
+        "STEEM", "STG", "STMX", "STORJ", "STRK", "STX", "SUI", "SUN", "SUPER", "SUSHI", "SXP", "SYN", 
+        "SYS", "TAO", "THETA", "TIA", "TLM", "TNSR", "TON", "TRB", "TRU", "TRX", "TWT", "UMA", "UNFI", 
+        "UNI", "USTC", "VANRY", "VET", "VIDT", "VOXEL", "WAXP", "WIF", "WLD", "WOO", "W", "XAI", "XEC", 
+        "XEM", "XLM", "XRP", "XTZ", "XVG", "XVS", "YFI", "YGG", "ZEC", "ZIL", "ZK", "ZRO", "ZRX"]; //outez d'autres symboles crypto si nécessaire
     cryptoSymbols.forEach((symbol) => fetchCryptoData(symbol));
 
     mettreAJourHeure();
