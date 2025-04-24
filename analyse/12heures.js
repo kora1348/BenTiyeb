036e296a11d7af559f5a235d7dd943415a2ab894
kora1348 <amelianekor@gmail.com>
2025-04-24 10:20:25 +0200

    toto

--- analyse/12heures.js
+++ analyse/12heures.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
     try {
       const response = await fetch(
-        `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=12h&limit=2`
+        `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=12h&limit=4`
       );
       const data = await response.json();
   
