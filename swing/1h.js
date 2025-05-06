92d45896caf163a2ce6109d794570c5eaa869dd0
kora1348 <amelianekor@gmail.com>
2025-05-06 21:37:20 +0200

    toto

--- swing/1h.js
+++ swing/1h.js
@@ -1,7 +1,7 @@
 async function fetchCryptoData(symbol) {
   try {
     const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1H&limit=2`
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1h&limit=2`
     );
     const data = await response.json();
 
